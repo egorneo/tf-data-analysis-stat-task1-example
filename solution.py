40ad073050ac6d3f231590896dd23ed379231ee1
egorneo <110398447+egorneo@users.noreply.github.com>
2023-10-18 17:37:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,6 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = x - 335
+    est = np.log(np.median(x))
+    return est + 335 # Ваш ответ
